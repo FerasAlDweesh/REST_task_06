19f57d9dae8952adc7ff089feab2b1b6cfe5f84c
Feras AlDweesh <ferasaldweesh@gmail.com>
2019-10-22 19:14:33 +0300

    Hesham and Feras

---
+++ flights/permissions.py
@@ -0,0 +1,20 @@
+from rest_framework.permissions import BasePermission
+from datetime import date
+
+
+class IsBookingUser(BasePermission):
+	
+	def has_object_permission(self, request, view, obj):
+		if request.user == obj.user or request.user.is_staff:
+			return True
+		return False
+
+
+class IsWithinThreeDays(BasePermission):
+
+	def has_object_permission(self, request, view, obj):
+		if (obj.date - date.today()).days > 3:
+			return True
+		return False
+
+
--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView, CreateAPIView
 from datetime import datetime
-
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsBookingUser, IsWithinThreeDays
 from .models import Flight, Booking
 from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer, RegisterSerializer, AdminUpdateBookingSerializer
 
@@ -12,6 +13,7 @@ class FlightsList(ListAPIView):
 
 class BookingsList(ListAPIView):
 	serializer_class = BookingSerializer
+	permission_classes = [IsAuthenticated]
 
 	def get_queryset(self):
 		return Booking.objects.filter(user=self.request.user, date__gte=datetime.today())
@@ -20,14 +22,18 @@ class BookingsList(ListAPIView):
 class BookingDetails(RetrieveAPIView):
 	queryset = Booking.objects.all()
 	serializer_class = BookingDetailsSerializer
+	permission_classes = [IsAuthenticated, IsBookingUser]
 	lookup_field = 'id'
 	lookup_url_kwarg = 'booking_id'
+	
 
 
 class UpdateBooking(RetrieveUpdateAPIView):
 	queryset = Booking.objects.all()
+	permission_classes = [IsAuthenticated, IsBookingUser, IsWithinThreeDays]
 	lookup_field = 'id'
 	lookup_url_kwarg = 'booking_id'
+	
 
 	def get_serializer_class(self):
 		if self.request.user.is_staff:
@@ -38,12 +44,15 @@ class UpdateBooking(RetrieveUpdateAPIView):
 
 class CancelBooking(DestroyAPIView):
 	queryset = Booking.objects.all()
+	permission_classes = [IsAuthenticated, IsBookingUser, IsWithinThreeDays]
 	lookup_field = 'id'
 	lookup_url_kwarg = 'booking_id'
+	
 
 
 class BookFlight(CreateAPIView):
 	serializer_class = AdminUpdateBookingSerializer
+	permission_classes = [IsAuthenticated]
 
 	def perform_create(self, serializer):
 		serializer.save(user=self.request.user, flight_id=self.kwargs['flight_id'])
